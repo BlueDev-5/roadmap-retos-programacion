3f60e2a47d69d634b9380e02b458ade8acf8d7dd
BlueDev-5 <Saturn945@proton.me>
2025-10-25 22:35:37 -0300

    Create BlueDev-5.py
    Aguante river

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BlueDev-5.py
@@ -0,0 +1,33 @@
+# https://www.python.org/ 
+#esto es un cometario de una linea
+
+""""
+esto es 
+un comentario
+en varias lieneas
+"""""
+'''''
+este es 
+otro ejemplo
+de comentarion
+en varias lineas
+'''  
+variable= "N1"
+variable= "Nuevo valor de N1"
+
+CONSTANTE= "Constante N1" # por convención
+
+int= 5
+float= 0.5
+bool_V = True
+bool_F=  False
+str_N1= "Que rico el pure de papa con costeleta"
+str_N2= 'Bajen el precio de la yerba mrd'
+print("Hello, Python!")
+
+print(type(int))
+print(type(float))
+print(type(bool_V))
+print(type(bool_F))
+print(type(str_N1))
+print(type(str_N2))
